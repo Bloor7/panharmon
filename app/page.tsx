81125d45ea2278e4ae230b294abeda387b67a43a
Tsix <huyetnguyet715@gmail.com>
2026-04-03 13:27:51 +0700

    updaate before vide code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturesSection from "@/components/section/FeaturesSection";
 import HeroSection from "@/components/section/HeroSection";
+import IntroSection from "@/components/section/IntroSection";
 
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <IntroSection />
       <FeaturesSection />
     </>
   );
